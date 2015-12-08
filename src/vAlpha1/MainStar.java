5449affd419566d2b18c1df92d63282fe67b12e8
gajdp9323 <gajdp9323@FHC-IC0026786.wrdsb.ca>
2015-12-08 09:05:23 -0500

    Starting

--- src/vAlpha1/MainStar.java
+++ src/vAlpha1/MainStar.java
@@ -1,19 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+//Creator's Philip G. and Samuel M.
+
 package vAlpha1;
 
-/**
- *
- * @author gajdp9323
- */
 public class MainStar {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
