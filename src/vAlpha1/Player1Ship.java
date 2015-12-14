953367eaca404e2c4d2fa45a9d09d9db7e593cf1
gajdp9323 <gajdp9323@FHC-IC0026786.wrdsb.ca>
2015-12-14 08:21:33 -0500

    Ships moving

--- src/vAlpha1/Player1Ship.java
+++ src/vAlpha1/Player1Ship.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package vAlpha2;
+package vAlpha1;
 
 /**
  *
  * @author gajdp9323
  */
-public class Laser {
+public class Player1Ship {
     
 }
