2e291ea5b6ae8476553e4b34246aabc49e9ffc69
gajdp9323 <gajdp9323@FHC-IC0026786.wrdsb.ca>
2015-12-14 08:48:22 -0500

    Ships moving and Lasers

--- src/vAlpha1/Player1Ship.java
+++ src/vAlpha1/Player1Ship.java
@@ -9,5 +9,11 @@ package vAlpha1;
  * @author gajdp9323
  */
 public class Player1Ship {
-    
+    int nAccel, nMaxSpeed, nRotateNegativeAccel, nRotateSpeed;
+    public Player1Ship() {
+        //KeyEvent event
+        while (1 == 1) {
+            //if (key 'w' pressed)
+        }
+    }
 }
