02a9ccf465e078c13e5a2d10c83d07e679fc6c39
gajdp9323 <gajdp9323@FHC-IC0026786.wrdsb.ca>
2015-12-14 08:30:08 -0500

    Ships moving and Lasers

---
+++ src/vAlpha1/Laser.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vAlpha1;
+
+/**
+ *
+ * @author gajdp9323
+ */
+public class Laser {
+    
+}
