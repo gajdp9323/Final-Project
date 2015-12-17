8d4420811f2108136446849815c21b9cd95e9fd5
gajdp9323 <gajdp9323@FHC-IC0026786.wrdsb.ca>
2015-12-17 08:58:08 -0500

    Ships moving and Lasers (still trying got key events) new minimum window size!!! (stats updated)

--- src/vAlpha1/Player1Ship.java
+++ src/vAlpha1/Player1Ship.java
@@ -12,24 +12,33 @@ import java.awt.event.*;
  */
 public class Player1Ship {
     Image imgPlayer1 = Toolkit.getDefaultToolkit().createImage("JavaShipP1.png");
-    double dAccel, dMaxSpeed, dRotateNegativeAccel, dRotateSpeed = 0.1;
+    double dAccel, dMaxSpeed, dRotateNegativeAccel, dRotateSpeed = 0.1, dShipAngle;
     Graphics g;
     public Player1Ship(KeyEvent e) {
         //KeyEvent event
         int KeyCode =e.getKeyCode();
-        while (1 == 1) {
+        while (true) {
+            if (dShipAngle>=360) dShipAngle-=360;
+            else if (dShipAngle<0) dShipAngle+=360;
             g.drawImage(imgPlayer1, 0, 0, null);
             if(KeyCode == KeyEvent.VK_W){
-                
+                if (dShipAngle>=0 && dShipAngle<90) {
+                    
+                }
+                else if (dShipAngle>270) {
+                    
+                }
             }
             if(KeyCode == KeyEvent.VK_S){
-           	 
+           	if (dShipAngle!=270) {
+                    
+                }
             }
             if(KeyCode == KeyEvent.VK_A){
-           	 
+           	dShipAngle += dRotateSpeed;
             }
             if(KeyCode == KeyEvent.VK_D){
-           	 
+                dShipAngle -= dRotateSpeed;
             }
             //if (key 'w' pressed)
         }
