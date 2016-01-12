b76ad859c9e5a8deb6739d411afe7cce56b84256
gajdp9323 <gajdp9323@FHC-IC0026786.wrdsb.ca>
2016-01-12 09:16:16 -0500

    keylistenertest update draws images (cortesy on sam)

--- src/Minigame/KeyListenerTest.java
+++ src/Minigame/KeyListenerTest.java
@@ -1,18 +1,20 @@
 package Minigame;
-
+//Purpose of code is to sample movement and make score increase when certain conditions are met
 //Philip G
 import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.Graphics;
 import java.awt.Color;
-//import java.awt.Image;
-//import java.awt.Toolkit;
+import java.awt.Image;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 class KeyListenerTester extends JFrame implements KeyListener {
+    Image imgPlayer1 = Toolkit.getDefaultToolkit().createImage("JavaShipP1.png");
+    Image imgPlayer2 = Toolkit.getDefaultToolkit().createImage("JavaShipP2.png");
     JLabel label;
     static int nX1 = 300, nY1 = 300, nX2 = 100, nY2 = 100, nScore = 0, nX1Temp = nX1, nX2Temp = nX2, nY1Temp = nY1, nY2Temp = nY2;
     public KeyListenerTester(String s) {
@@ -84,7 +86,7 @@ class KeyListenerTester extends JFrame implements KeyListener {
             System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
             System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
             System.out.println("Score: " + nScore);
-            if (nX1Temp<(nX1-5) || nX1Temp>(nX1+5) || nX2Temp<(nX2-5) || nX2Temp>(nX2+5)) {
+            if (nX1Temp<(nX1-10) || nX1Temp>(nX1+10) || nX2Temp<(nX2-10) || nX2Temp>(nX2+10)) {
                 nScore+=100;
                 nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2; //to shorten code since this in repeated alot
             }
@@ -95,7 +97,7 @@ class KeyListenerTester extends JFrame implements KeyListener {
             System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
             System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
             System.out.println("Score: " + nScore);
-            if (nX1Temp<(nX1-5) || nX1Temp>(nX1+5) || nX2Temp<(nX2-5) || nX2Temp>(nX2+5)) {
+            if (nX1Temp<(nX1-10) || nX1Temp>(nX1+10) || nX2Temp<(nX2-10) || nX2Temp>(nX2+10)) {
                 nScore+=100;
                 nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
             }
@@ -106,7 +108,7 @@ class KeyListenerTester extends JFrame implements KeyListener {
             System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
             System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
             System.out.println("Score: " + nScore);
-            if (nX1Temp<(nX1-5) || nX1Temp>(nX1+5) || nX2Temp<(nX2-5) || nX2Temp>(nX2+5)) {
+            if (nX1Temp<(nX1-10) || nX1Temp>(nX1+10) || nX2Temp<(nX2-10) || nX2Temp>(nX2+10)) {
                 nScore+=100;
                 nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
             }
@@ -117,7 +119,7 @@ class KeyListenerTester extends JFrame implements KeyListener {
             System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
             System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
             System.out.println("Score: " + nScore);
-            if (nX1Temp<(nX1-5) || nX1Temp>(nX1+5) || nX2Temp<(nX2-5) || nX2Temp>(nX2+5)) {
+            if (nX1Temp<(nX1-10) || nX1Temp>(nX1+10) || nX2Temp<(nX2-10) || nX2Temp>(nX2+10)) {
                 nScore+=100;
                 nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
             }
@@ -128,7 +130,7 @@ class KeyListenerTester extends JFrame implements KeyListener {
             System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
             System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
             System.out.println("Score: " + nScore);
-            if (nY1Temp<(nY1-5) || nY1Temp>(nY1+5) || nY2Temp<(nY2-5) || nY2Temp>(nY2+5)) {
+            if (nY1Temp<(nY1-10) || nY1Temp>(nY1+10) || nY2Temp<(nY2-10) || nY2Temp>(nY2+10)) {
                 nScore+=100;
                 nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
             }
@@ -139,7 +141,7 @@ class KeyListenerTester extends JFrame implements KeyListener {
             System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
             System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
             System.out.println("Score: " + nScore);
-            if (nY1Temp<(nY1-5) || nY1Temp>(nY1+5) || nY2Temp<(nY2-5) || nY2Temp>(nY2+5)) {
+            if (nY1Temp<(nY1-10) || nY1Temp>(nY1+10) || nY2Temp<(nY2-10) || nY2Temp>(nY2+10)) {
                 nScore+=100;
                 nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
             }
@@ -150,7 +152,7 @@ class KeyListenerTester extends JFrame implements KeyListener {
             System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
             System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
             System.out.println("Score: " + nScore);
-            if (nY1Temp<(nY1-5) || nY1Temp>(nY1+5) || nY2Temp<(nY2-5) || nY2Temp>(nY2+5)) {
+            if (nY1Temp<(nY1-10) || nY1Temp>(nY1+10) || nY2Temp<(nY2-10) || nY2Temp>(nY2+10)) {
                 nScore+=100;
                 nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
             }
@@ -161,7 +163,7 @@ class KeyListenerTester extends JFrame implements KeyListener {
             System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
             System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
             System.out.println("Score: " + nScore);
-            if (nY1Temp<(nY1-5) || nY1Temp>(nY1+5) || nY2Temp<(nY2-5) || nY2Temp>(nY2+5)) {
+            if (nY1Temp<(nY1-10) || nY1Temp>(nY1+10) || nY2Temp<(nY2-10) || nY2Temp>(nY2+10)) {
                 nScore+=100;
                 nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
             }
@@ -169,18 +171,16 @@ class KeyListenerTester extends JFrame implements KeyListener {
         }
     }
     public void paint(Graphics g) {
-        //Image imgPlayer1 = Toolkit.getDefaultToolkit().createImage("JavaShipP1.png"); //Tried to import images but drawing them failed
-        //Image imgPlayer2 = Toolkit.getDefaultToolkit().createImage("JavaShipP2.png"); //comented out to hopefully use in the future
         revalidate();
         repaint();
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, 2000, 2000);
-        g.setColor(Color.RED);
-        g.fillRect (nX1, nY1, 5, 5);
-        g.setColor(Color.BLUE);
-        g.fillRect (nX2, nY2, 5, 5);
-        //g.drawImage(imgPlayer2, nX2, nY2, null); //image was not displayed
-        //g.drawImage(imgPlayer1, nX1, nY1, null); //image was not displayed
+        //g.setColor(Color.RED);    images work
+        //g.fillRect (nX1, nY1, 5, 5);
+        //g.setColor(Color.BLUE);
+        //g.fillRect (nX2, nY2, 5, 5);
+        g.drawImage(imgPlayer1, nX1-(imgPlayer1.getWidth(rootPane)/2), nY1-(imgPlayer1.getHeight(rootPane)/2), null);
+        g.drawImage(imgPlayer2, nX2-(imgPlayer2.getWidth(rootPane)/2), nY2-(imgPlayer2.getHeight(rootPane)/2), null);
   }
     public static void main(String[] args) {
         new KeyListenerTester("Key Listener Tester");
